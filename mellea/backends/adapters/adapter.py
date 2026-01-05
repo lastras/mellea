ff4e5bc234bb272a7d453bd1ef16716b16ebe96c
jakelorocco <59755218+jakelorocco@users.noreply.github.com>
2026-01-05 14:46:51 -0500

    fix: unpin granite_commons version from 0.3.5 (#287)

--- mellea/backends/adapters/adapter.py
+++ mellea/backends/adapters/adapter.py
@@ -134,8 +134,8 @@ class GraniteCommonAdapter(OpenAIAdapter, LocalHFAdapter):
             config_file = granite_common.intrinsics.obtain_io_yaml(
                 self.intrinsic_name,
                 self.base_model_name,
+                self.intrinsic_metadata.repo_id,
                 alora=is_alora,
-                repo_id=self.intrinsic_metadata.repo_id,
             )
         if config_file:
             with open(config_file, encoding="utf-8") as f:
@@ -201,8 +201,8 @@ class GraniteCommonAdapter(OpenAIAdapter, LocalHFAdapter):
             granite_common.intrinsics.obtain_lora(
                 self.intrinsic_name,
                 base_model_name,
+                self.intrinsic_metadata.repo_id,
                 alora=is_alora,
-                repo_id=self.intrinsic_metadata.repo_id,
             )
         )
 
